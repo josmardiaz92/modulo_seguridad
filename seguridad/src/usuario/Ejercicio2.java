4df5d9159fc73a3d6fb88e3b042ef94adb74b6fe
josmardiaz92 <josmardiaz92@gmail.com>
2023-09-04 21:00:10 -0400

    primer objeto creado

---
+++ seguridad/src/usuario/Ejercicio2.java
@@ -0,0 +1,54 @@
+package usuario;
+
+class Rol{
+    int cod_rol;
+    String nom_rol;
+    String des_rol;
+    String est_rol;
+    public Rol(int cod_rol) {
+        this.cod_rol = cod_rol;
+    }
+    public Rol(String nom_rol, String des_rol, String est_rol) {
+        this.nom_rol = nom_rol;
+        this.des_rol = des_rol;
+        this.est_rol = est_rol;
+    }
+    public Rol(int cod_rol, String nom_rol, String des_rol, String est_rol) {
+        this.cod_rol = cod_rol;
+        this.nom_rol = nom_rol;
+        this.des_rol = des_rol;
+        this.est_rol = est_rol;
+    }
+    public int getCod_rol() {
+        return cod_rol;
+    }
+    public void setCod_rol(int cod_rol) {
+        this.cod_rol = cod_rol;
+    }
+    public String getNom_rol() {
+        return nom_rol;
+    }
+    public void setNom_rol(String nom_rol) {
+        this.nom_rol = nom_rol;
+    }
+    public String getDes_rol() {
+        return des_rol;
+    }
+    public void setDes_rol(String des_rol) {
+        this.des_rol = des_rol;
+    }
+    public String getEst_rol() {
+        return est_rol;
+    }
+    public void setEst_rol(String est_rol) {
+        this.est_rol = est_rol;
+    }
+}
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Rol r1=new Rol(1,"Administrador","Tienen todos los privilegios","A");
+        Rol r2=new Rol(2,"Propietario","No tiene todos los privilegios","A");
+        Rol r3=new Rol(3,"Propietario2","No tiene todos los privilegios","A");
+    }
+}
