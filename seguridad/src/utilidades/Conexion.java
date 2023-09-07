441d33c15daf24e52d049f6d31fd248c77607d00
josmardiaz92 <josmardiaz92@gmail.com>
2023-09-06 20:54:48 -0400

    conexion con la bd exitosa

---
+++ seguridad/src/utilidades/Conexion.java
@@ -0,0 +1,22 @@
+package utilidades;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+    Connection conex;
+    public Conexion(){
+        try {
+            conex=DriverManager.getConnection("jdbc:postgresql://localhost:5432/diplomado", "postgres", "240296");
+            System.out.println("BD Conectada");
+        } catch (SQLException ex) {
+            System.out.println("Error al conectar la BD: "+ex);
+        }
+    }
+    public Connection getConnection(){
+        return conex;
+    }//esto nos permite retornar el valor para saber que devuelve la conexion
+    public static void main(String[] args) {
+        Conexion c=new Conexion();
+        
+    }
+}
